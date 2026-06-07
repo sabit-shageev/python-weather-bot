15f4ecc14541f97f198778dd05ffeac811af8428
Karim Valeev <46091087+Karim-Valeev@users.noreply.github.com>
2026-06-07 20:17:41 +0300

    Add docker files (#13)

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}"
 
 # Имя файла базы данных
-DB_NAME = "users.db"
+DB_NAME = "src/db/users.db"
 
 # Состояния пользователя (FSM — упрощенная)
 STATE_WAITING_INFO = "waiting_for_info"
